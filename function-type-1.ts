f4d70fca96812199ba0aeb3c5eab8c2a95368b67
infinity <ef.jamshidi017@gmail.com>
2023-07-16 08:29:19 +0330

    adding some functions to function-type-1

--- function-type-1.ts
+++ function-type-1.ts
@@ -5,3 +5,24 @@ function test(name:string):string{
 function test2(name:string){
     return "hi dear: " + name;
 }
+
+// ts considers name variable as any type but return value type is string
+function test3(name){
+    return "hi dear: " + name;
+}
+
+// return type is string or number
+function test4(name:string){
+    if(name="Moeinnajafi1998"){
+        return name;
+    }
+    return Math.floor(Math.random());
+}
+
+// input and return are any type
+function test5(name){
+    if(name="Moeinnajafi1998"){
+        return name;
+    }
+    return Math.floor(Math.random());
+}
